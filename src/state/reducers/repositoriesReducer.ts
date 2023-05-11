4ad0ccbbc9f5de24cef25fcbcaf2ad0ed75ee597
Javier Blanco <javierblancoit@gmail.com>
2023-05-11 07:43:21 +0100

    add type annotation and move definitions to different files

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,21 +1,27 @@
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
 interface RepositoriesState {
   loading: boolean;
   error: string | null;
   data: string[];
 }
 
-const reducer = (state: RepositoriesState, action: any) => {
+const reducer = (
+  state: RepositoriesState,
+  action: Action
+): RepositoriesState => {
   switch (action.type) {
     // send new request :
-    case 'search_repositories':
+    case ActionType.SEARCH_REPOSITORIES:
       // set loading state on, and reset error and data from previous request
       return { loading: true, error: null, data: [] };
     // get succcesful response from npm API
-    case 'search_repositories_success':
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
       // set loading state off, and set error to null and data to info about npm repos
       return { loading: false, error: null, data: action.payload };
     // get error response from npm API
-    case 'search_repositories_error':
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
       // set loading state off, and set error to error message from npm API and reset data
       return { loading: false, error: action.payload, data: [] };
     default:
